4b2ca4eacfc09aa1e6cd7889fc527446079bda96
juanpinto1 <juan.pinto@cajalosandes.cl>
2024-08-07 21:38:30 -0400

    cambios

--- src/database/dbConfig.js
+++ src/database/dbConfig.js
@@ -1,14 +1,31 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_URL } = process.env;
 
-const database = new Pool({
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-    allowExitOnIdle: true
-});
+
+const database = DB_URL
+  ? new Pool({
+        connectionString: DB_URL,       
+        ssl: {
+            rejectUnauthorized: false,
+        },
+        allowExitOnIdle: true,
+    })
+  : new Pool({
+        host: DB_HOST,
+        user: DB_USER,
+        password: DB_PASSWORD,
+        database: DB_NAME,
+        allowExitOnIdle: true
+    });
+
+try {
+  /*Verifica la conexión*/
+  database.query("SELECT NOW()"); 
+  console.log("Database connected");  
+} catch (error) {
+  console.log(error);
+}
 
 module.exports = database;
