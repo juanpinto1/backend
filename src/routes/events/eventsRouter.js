0f60343ca69a34c1049daaece78bcf73bd9e668d
juanpinto1 <juan.pinto@cajalosandes.cl>
2024-08-12 21:04:13 -0400

    cambios para agregar update y delete

--- src/routes/events/eventsRouter.js
+++ src/routes/events/eventsRouter.js
@@ -10,11 +10,11 @@ const {
 } = require('../../controllers/Events/EventsController');
 
 router.get('/get-all', get_events_controller);
-router.get('/get/:id', get_Event_by_id);
+router.get('/get/:event_id', get_Event_by_id);
 router.post('/mis-eventos', authMiddleware, get_events_by_user); // Cambiado a POST y ruta actualizada
 
 router.post('/add', add_Event_controller);
-router.put('/update/:id', update_Event_controller);
-router.delete('/delete/:id', delete_Event_controller);
+router.put('/update/:event_id', update_Event_controller);
+router.delete('/delete/:event_id', delete_Event_controller);
 
 module.exports = router;
